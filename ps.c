08f4a47c3b6f36bd547d4d1810fd0e7a84e682f3
rufus <rufus.oelkers@arbourglobal.com>
2021-11-23 14:38:59 +0100

    implement basic ps utility

--- ps.c
+++ ps.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,8 +6,35 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <stdbool.h>
+#include <time.h>
 
 #define PROCDIR "/proc"
+#define WORDBUFLENGTH 100
+#define MAXWORDS 50
+
+void splitStr(char* input, char * words[MAXWORDS], int wordsSize, char separator) {
+  for(int j = 0; j < wordsSize -1; j++) {
+    words[j] = NULL;
+  }
+  int i = 0;
+  char c;
+  char * p;
+  words[i] = malloc(WORDBUFLENGTH);
+  p = words[i];
+  while((c = *input) != '\0') {
+    if(c == separator){
+      *p = '\0';
+      i++;
+      words[i] = malloc(WORDBUFLENGTH);
+      p = words[i];
+    } else {
+      *p = c;
+      p++;
+    }
+    input++;
+  }
+  p = '\0';
+}
 
 int isDirectory(const char * path) {
   struct stat statbuf;
@@ -14,14 +42,6 @@ int isDirectory(const char * path) {
     return 0;
   }
   return S_ISDIR(statbuf.st_mode);
-}
-
-int isLink(const char * path) {
-  struct stat statbuf;
-  if (lstat(path, &statbuf) != 0) {
-    return 0;
-  }
-  return S_ISLNK(statbuf.st_mode);
 }
 
 bool IsDigitsOnly(char * c) {
@@ -42,31 +62,39 @@ char * concatFolder(char * path, char * folder) {
   return absolute_path;
 }
 
-void checkTtyInProcessFd(DIR * directory_reader, char* absolute_path) {
-  struct dirent *directory;
-  char * filePath;
-  while ((directory= readdir(directory_reader)) != NULL) {
-    filePath = concatFolder(absolute_path, directory->d_name); 
-    printf("is file %s\n", filePath);
-    if(isLink(filePath)) {
-      printf("is Link %s\n", filePath);
-    }
+char * getTime(char * absolute_path) {
+  struct stat statbuf;
+  if (stat(absolute_path, &statbuf) != 0) {
+    return 0;
   }
+  __time_t uptime = statbuf.st_mtime - time(NULL);
+  struct tm time = *localtime(&uptime);
+  char * timeStr; 
+  asprintf(&timeStr, "%d:%d:%d", time.tm_hour, time.tm_min, time.tm_sec);
+  return timeStr;
 }
 
-void handleProcessDir(char * absolute_path) {
-  char * fdPath = concatFolder(absolute_path, "fd");
+char * getCmd(char * absolute_path) {
+  char * cmdPath = concatFolder(absolute_path, "status");
+  char * cmdColumns[3];
+  char * cmd = NULL;
+  size_t n;
   //Check if has permission to oper folder
-  if(access(fdPath, R_OK) != 0) {
-    return;
+  FILE *fp = fopen(cmdPath, "r");
+  if(fp == NULL) {
+    perror("fopen");
+    return "";
   }
-  /* printf("%s\n", fdPaths); */
-  DIR *directory_reader = opendir(fdPath); 
-  if (directory_reader == NULL) {
-    printf("Could not open current directory" );
-    exit(0);
-  }
-  checkTtyInProcessFd(directory_reader, fdPath);
+  getline(&cmd, &n, fp);
+  splitStr(cmd, cmdColumns, 3, '\t');
+  fclose(fp);
+  return cmdColumns[1];
+}
+
+void handleProcessDir(char * absolutePath, char * processId) {
+  char * cmd = getCmd(absolutePath);
+  char * time = getTime(absolutePath);
+  printf("%s\t%s\t%s", processId, time, cmd);
 }
 
 void readProcessDir(struct dirent * directory) {
@@ -74,7 +102,7 @@ void readProcessDir(struct dirent * directory) {
   if(IsDigitsOnly(directory->d_name) ) {
     char * absolute_path = concatFolder(PROCDIR, directory->d_name);
     if(isDirectory(absolute_path)) {
-      handleProcessDir(absolute_path);
+      handleProcessDir(absolute_path, directory->d_name);
     }
   }
 }
@@ -88,6 +116,7 @@ int main() {
         printf("Could not open current directory" ); 
         exit(0);
     }
+    printf("PID\tTIME\tCMD\n");
     while ((directory= readdir(directory_reader)) != NULL) {
       readProcessDir(directory);
     }
